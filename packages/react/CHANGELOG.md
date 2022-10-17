d584e011149c03a5dde033760eb4b1ddabffbb73
Milena Boselli Rosa <milena.rosa@gmail.com>
2022-10-17 18:43:29 -0300

    ci: add turborepo cache on deploy build

---
+++ packages/react/CHANGELOG.md
@@ -0,0 +1,7 @@
+# @mbr-ui/react
+
+## 1.1.0
+
+### Minor Changes
+
+- Add display name to all components.
--- packages/react/src/styles/index.ts
+++ packages/react/src/styles/index.ts
@@ -7,7 +7,7 @@ import {
   lineHeights,
   radii,
   space,
-} from '@ignite-ui/tokens'
+} from '@mbr-ui/tokens'
 
 export const {
   styled,
